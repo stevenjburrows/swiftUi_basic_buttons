32b50730a79e10aea829b1c00c229d3943e5b167
steven <steven@burrows.plus.com>
2020-05-15 10:37:28 +0100

    Initial Commit

---
+++ basic_buttons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basic_buttons
+//
+//  Created by Steven Burrows on 15/05/2020.
+//  Copyright © 2020 Steven Burrows. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
